7ea565d1ce8873fb64643540a1f20093b67331fd
初夏的蝉︶° <i@wokao.in>
2017-05-30 12:25:07 +0800

    第一个

---
+++ 2017-05-27-mylearn1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2017_05_27_mylearn1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
